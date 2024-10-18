13ac22e351ccaabe57ccd3272f18d19c4ca498af
Matheus Fiebig <macrubig@gmail.com>
2024-10-18 17:50:56 -0300

    test throw

--- src/test/java/com/mock/NumberAscOrderTest.java
+++ src/test/java/com/mock/NumberAscOrderTest.java
@@ -1,6 +1,7 @@
 package com.mock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -14,13 +15,37 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class NumberAscOrderTest {
-    private NumberAscOrder<Double> numberAscOrder;
+    private NumberAscOrder<Double> cut;
 
     @Mock CalculableStrategy<Double> calculableStrategy;
     @Mock CustomStack<Double> stack;
 
     @BeforeEach
     public void setup() {
-        numberAscOrder = new NumberAscOrder<Double>(stack);
+        stack = new CustomStack<>(6, calculableStrategy);
+        cut = new NumberAscOrder<Double>(stack);
+    }
+
+    @Test
+    public void shouldSort() throws StackEmptyException, StackFullException{
+        var expected = List.of(Math.random(), Math.random(), Math.random(), Math.random(), Math.random(), Math.random());
+
+        for(int i = 0; i < expected.size(); i ++){
+            when(calculableStrategy.calculateValue(Mockito.anyDouble()))
+                .thenReturn(expected.get(i));
+            
+            stack.push(expected.get(i));
+        }
+
+        var actual = cut.sort();
+
+        assertEquals(expected.stream().sorted().toList(), actual);
+    }
+
+    @Test
+    public void shouldThrowWhenEmpty() throws StackEmptyException, StackFullException{
+        assertThrows(StackEmptyException.class, () -> {
+            cut.sort();
+        });
     }
 }
